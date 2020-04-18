68c525719da88ee26f93a7e7d2667cb869fee5e7
shtdfck <63334197+shtdfck@users.noreply.github.com>
2020-04-19 01:04:13 +0800

    membuat button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Button = ({ text, onClick, color, align }) => {
+  const classNames = [
+    "header-btn",
+    color === "black" && "main-black-color",
+    color === "red" && "main-red-color",
+    align === "right" && "align-right",
+    align === "left" && "align-left"
+  ].join(" ");
+
+  return (
+    <button className={classNames} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  text: "Button",
+  color: "Black",
+  align: "left"
+};
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  color: PropTypes.oneOf(["black", "red"]),
+  align: PropTypes.oneOf(["left", "right"])
+};
+
+export default Button;
